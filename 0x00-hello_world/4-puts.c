888361ae235b6c8cf9cdf8574dac7cef3fca653d
youssef-hassane <youssef_hassane@icloud.com>
2023-10-05 19:39:06 +0300

    answering task number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,4 @@
-/*
- *  4-puts.c
- * 
- */
-
 #include <stdio.h>
-
-/**
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, 
- * followed by a new line.
- *
- * Return 0
- */
 
 int main(void) 
 {
