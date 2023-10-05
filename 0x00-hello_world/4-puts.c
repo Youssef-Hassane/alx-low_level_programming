37d27906bf8bd099854d5b712f7c3310153776aa
youssef-hassane <youssef_hassane@icloud.com>
2023-10-05 19:55:11 +0300

    answering task number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
+/*
+ * Youssef Hassane 
+ * 
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * puts "Programming is like building a multilingual puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-int main(void) 
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
