c31f378e642d96d6439b13e4f77db19d0f6a67f4
youssef-hassane <youssef_hassane@icloud.com>
2023-10-07 02:37:17 +0300

    answering task number 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -19,7 +19,7 @@ int main(void)
 char *alphabet[] = {
 "a", "b", "c", "d", "e", "f", "g", "h", "i",
 "j", "k", "l", "m", "n", "o", "p", "q", "r",
-"s", "t", "u", "v", "w", "x", "y", "z"
+"s", "t", "u", "v", "w", "x", "y", "z", "\0",
 };
 /* Obtaining the length of array */
 int lengthOfArray = sizeof(alphabet) / sizeof(alphabet[0]);
