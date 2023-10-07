a64ef7d5a41326594bd6e284d0c130fe297abca6
youssef-hassane <youssef_hassane@icloud.com>
2023-10-07 19:45:06 +0300

    answering task number 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * --------------------------
+ * Write a program that prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ * --------------------------
+ * By Youssef Hassane
+ */
+int main(void)
+{
+int number;
+char character;
+
+for (number = 0; number < 10; number++)
+{
+putchar((number % 10) + '0');
+}
+for (character = 'a'; character <= 'f'; character++)
+{
+putchar(character);
+}
+putchar('\n');
+
+return (0);
+}
