70d7c8024e9775fa749f4370413ad5fd7e92989b
youssef-hassane <youssef_hassane@icloud.com>
2023-10-07 03:59:40 +0300

    answering task number 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* Adding the stdio.h library so that I can utilize the printf */
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -12,32 +10,24 @@
  * --------------------------
  * By Youssef Hassane
  */
-
 int main(void)
 {
 /* An array of all the alphabet characters */
-char *alphabet[] = {
-"a", "b", "c", "d", "e", "f", "g", "h", "i",
-"j", "k", "l", "m", "n", "o", "p", "q", "r",
-"s", "t", "u", "v", "w", "x", "y", "z",
+char *alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i",
+"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 };
-char *alphabetUppercase[] = {
-"A", "B", "C", "D", "E", "F", "G", "H", "I", 
-"J", "K", "L", "M", "N", "O", "P", "Q", "R", 
-"S", "T", "U", "V", "W", "X", "Y", "Z"
+char *alphabetUppercase[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", 
+"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 };
-
 /* Obtaining the length of array */
 int lengthOfArray = sizeof(alphabet) / sizeof(alphabet[0]);
 /* Obtaining the length of array */
 int lengthOfArrayUppercase = sizeof(alphabetUppercase) / sizeof(alphabetUppercase[0]);
-
 /* Utilizing the for loop in order to print the alphabet characters */
-int i;
-int n;
+int i, n;
 for (i = 0; i < lengthOfArray; i++)
 {
-const char *eachCharacter = alphabet[i];
+ const char *eachCharacter = alphabet[i];
 putchar(*eachCharacter);
 }
 for (n = 0; n < lengthOfArrayUppercase; n++)
@@ -46,7 +36,7 @@ const char *eachCharacter2 = alphabetUppercase[n];
 putchar(*eachCharacter2);
 }
 putchar('\n');
-
+return (0);
 /* =========================================  */
 /**
  * Another Way - to prints all the alphabet characters:
@@ -69,5 +59,5 @@ putchar('\n');
 */
 /* =========================================  */
 
-return (0);
+
 }
