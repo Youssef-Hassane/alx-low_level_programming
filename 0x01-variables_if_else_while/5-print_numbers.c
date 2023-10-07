e9b5f965cb3c9a28c33f29f859edb25a8f886917
youssef-hassane <youssef_hassane@icloud.com>
2023-10-07 05:29:48 +0300

    answering task number 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * --------------------------
+ * Write a program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ * --------------------------
+ * By Youssef Hassane
+ */
+int main(void)
+{
+char charactersOfAlphabet;
+/*
+ * Utilizing the for loop in order to all single digit numbers
+ * of base 10 starting from 0
+ */
+
+int num:
+for (num = 0; num < 10; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
