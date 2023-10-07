84732aa9223488496e5d66328b62becac412d3dd
youssef-hassane <youssef_hassane@icloud.com>
2023-10-07 04:03:35 +0300

    answering task number 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,28 +10,20 @@
  */
 int main(void)
 {
-/* An array of all the alphabet characters */
-char *alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i",
-"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-};
-char *alphabetUppercase[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", 
-"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-};
-/* Obtaining the length of array */
-int lengthOfArray = sizeof(alphabet) / sizeof(alphabet[0]);
-/* Obtaining the length of array */
-int lengthOfArrayUppercase = sizeof(alphabetUppercase) / sizeof(alphabetUppercase[0]);
+char charactersOfTheAlphabet;
 /* Utilizing the for loop in order to print the alphabet characters */
-int i, n;
-for (i = 0; i < lengthOfArray; i++)
+for (charactersOfTheAlphabet = 'a';
+charactersOfTheAlphabet <= 'z';
+charactersOfTheAlphabet++)
 {
- const char *eachCharacter = alphabet[i];
-putchar(*eachCharacter);
+putchar(charactersOfTheAlphabet);
 }
-for (n = 0; n < lengthOfArrayUppercase; n++)
+/* Utilizing the for loop in order to print the alphabet characters */
+for (charactersOfTheAlphabet = 'A';
+charactersOfTheAlphabet <= 'Z';
+charactersOfTheAlphabet++)
 {
-const char *eachCharacter2 = alphabetUppercase[n];
-putchar(*eachCharacter2);
+putchar(charactersOfTheAlphabet);
 }
 putchar('\n');
 return (0);
