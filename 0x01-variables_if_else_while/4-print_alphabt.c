e34ae141b600f7777ab57b9c03e7e4139102346c
youssef-hassane <youssef_hassane@icloud.com>
2023-10-07 05:24:40 +0300

    answering task number 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * --------------------------
+ * Write a program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Print all the letters except q and e
+ * --------------------------
+ * By Youssef Hassane
+ */
+int main(void)
+{
+char charactersOfAlphabet;
+/* Utilizing the for loop in order to print the alphabet characters */
+for (charactersOfAlphabet = 'a';
+charactersOfAlphabet <= 'z';
+charactersOfAlphabet++)
+{
+/* Print all the letters except q and e */
+if (charactersOfAlphabet != "q" && charactersOfAlphabet != "e")
+{
+putchar(charactersOfAlphabet);
+}
+}
+putchar('\n');
+return (0);
+}
