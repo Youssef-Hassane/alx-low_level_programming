bc7caf6a5b64f4b30794114138dbeaa6c778ad3d
youssef-hassane <youssef_hassane@icloud.com>
2023-10-08 01:07:39 +0300

    answering task number 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,28 +14,27 @@ int main(void)
 {
 int firstNumber, secondNumber, thirdNumber;
 
-for (firstNumber = 0; firstNumber < 9; firstNumber++)
+for (firstNumber = 0; firstNumber < 8; firstNumber++)
 {
-
 secondNumber = firstNumber + 1;
-while (secondNumber < 10)
+while (secondNumber < 9)
 {
 for (thirdNumber = secondNumber + 1; thirdNumber < 10; thirdNumber++)
 {
 putchar((firstNumber % 10) + '0');
 putchar((secondNumber % 10) + '0');
 putchar((thirdNumber % 10) + '0');
+
 if (firstNumber == 7 && secondNumber == 8 && thirdNumber == 9)
-{
 break;
-}
+
 putchar(',');
 putchar(' ');
-
+}
+secondNumber++;
 }
 }
 putchar('\n');
 
 return (0);
-}
 }
