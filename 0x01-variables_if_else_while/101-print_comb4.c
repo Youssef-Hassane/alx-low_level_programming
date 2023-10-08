eb0cd710f066f8029738d2d8db9e47ba58dfc395
youssef-hassane <youssef_hassane@icloud.com>
2023-10-08 15:01:29 +0300

    Fixing betty in task number 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,29 +12,29 @@
 
 int main(void)
 {
-int firstNumber, secondNumber, thirdNumber;
+	int firstNumber, secondNumber, thirdNumber;
 
-for (firstNumber = 0; firstNumber < 8; firstNumber++)
-{
-secondNumber = firstNumber + 1;
-while (secondNumber < 9)
-{
-for (thirdNumber = secondNumber + 1; thirdNumber < 10; thirdNumber++)
-{
-putchar((firstNumber % 10) + '0');
-putchar((secondNumber % 10) + '0');
-putchar((thirdNumber % 10) + '0');
+	for (firstNumber = 0; firstNumber < 8; firstNumber++)
+	{
+		secondNumber = firstNumber + 1;
+		while (secondNumber < 9)
+		{
+			for (thirdNumber = secondNumber + 1; thirdNumber < 10; thirdNumber++)
+			{
+				putchar((firstNumber % 10) + '0');
+				putchar((secondNumber % 10) + '0');
+				putchar((thirdNumber % 10) + '0');
 
-if (firstNumber == 7 && secondNumber == 8 && thirdNumber == 9)
-break;
+				if (firstNumber == 7 && secondNumber == 8 && thirdNumber == 9)
+					break;
 
-putchar(',');
-putchar(' ');
-}
-secondNumber++;
-}
-}
-putchar('\n');
+				putchar(',');
+				putchar(' ');
+			}
+			secondNumber++;
+		}
+	}
+	putchar('\n');
 
-return (0);
+	return (0);
 }
