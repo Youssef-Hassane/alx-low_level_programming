cca1423c0f9cd18ea9c612202497f0201519ad73
youssef-hassane <youssef_hassane@icloud.com>
2023-10-08 13:47:00 +0300

    Fixing betty in task number 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -20,7 +20,7 @@ int main(void)
 	"a", "b", "c", "d", "e", "f", "g", "h", "i",
 	"j", "k", "l", "m", "n", "o", "p", "q", "r",
 	"s", "t", "u", "v", "w", "x", "y", "z",
-	}; 
+	};
 	/* Obtaining the length of array */
 	int lengthOfArray = sizeof(alphabet) / sizeof(alphabet[0]);
 	/* Utilizing the for loop in order to print the alphabet characters */
