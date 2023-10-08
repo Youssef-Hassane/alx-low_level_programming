d479b280ba863fa3387027237817fd517b32fe89
youssef-hassane <youssef_hassane@icloud.com>
2023-10-08 15:00:26 +0300

    Fixing betty in task number 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,18 +12,18 @@
 
 int main(void)
 {
-int number;
+	int number;
 
-for (number = 0; number <= 9; number++)
-{
-putchar((number % 10) + '0');
-if (number == 9)
-{
-break;
-}
-putchar(',');
-putchar(' ');
-}
-putchar('\n');
-return (0);
+	for (number = 0; number <= 9; number++)
+	{
+		putchar((number % 10) + '0');
+		if (number == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
