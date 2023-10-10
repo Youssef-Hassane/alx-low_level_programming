5ce507c3dc869640de74cae4cde1b656486133ac
youssef-hassane <youssef_hassane@icloud.com>
2023-10-10 16:56:39 +0300

    Answering task number 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,11 +14,12 @@ int main(void)
 {
 	char putChar[] = "_putchar";
 	int num;
-
+	/* This for loop takes each character/element of putChar[] and print it */
 	for (num = 0; num <= 7; num++)
 	{
 		putchar(putChar[num]);
 	}
+	/* adding a new line after printing "_putchar" */
 	putchar('\n');
 	return (0);
 }
