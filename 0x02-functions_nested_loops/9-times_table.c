eb6fc0656c8b2cde3bb3a13c4b773fc96c87fcac
youssef-hassane <youssef_hassane@icloud.com>
2023-10-10 23:49:01 +0300

    Answering task number 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Write a function that prints the 9 times table
+ * starting with 0.
+ *
+ *
+ * Return: the last digit of a number
+ * --------------------------
+ * Write a function that prints the 9 times table, starting with 0.
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+void times_table(void)
+{
+	int number, multi, result;
+
+	for (number = 0; number < 10; number++)
+	{
+		for (multi = 0; multi <= 9; multi++)
+		{
+			result = (number * multi);
+			if (multi != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (result < 10 && multi != 0)
+			{
+				_putchar(' ');
+				_putchar((result % 10) + '0');
+			}
+			else
+			{
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
