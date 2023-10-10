29199e6ebc4c8af6229827fb59f8d573851873f2
youssef-hassane <youssef_hassane@icloud.com>
2023-10-10 17:38:28 +0300

    Answering task number 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
-#include "main.h"
 #include <stdio.h>
+#include "function/main.h"
+
 /**
- * main - Entry point
+ * print_alphabet - prints the english alphabet from a-z
  * Return: Always 0 (Success)
  * --------------------------
  * Write a function that prints the alphabet, in lowercase, followed by
@@ -11,17 +12,14 @@
  * --------------------------
  * By Youssef Hassane
  */
-int main(void)
-{
-	char charactersOfTheAlphabet;
 
-	/* Utilizing the for loop in order to print the alphabet characters */
-	for (charactersOfTheAlphabet = 'a';
-	     charactersOfTheAlphabet <= 'z';
-	     charactersOfTheAlphabet++)
+void print_alphabet(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
 	{
-		_putchar(charactersOfTheAlphabet);
+		_putchar(character);
 	}
 	_putchar('\n');
-	return (0);
 }
