48052ede87336f5d69346035d5711ee981b288f8
youssef-hassane <youssef_hassane@icloud.com>
2023-10-10 19:22:04 +0300

    Answering task number 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -20,24 +20,26 @@
 int print_sign(int num)
 {
 
-	if (num > 0)
+	switch (num)
 	{
-		_putchar('+');
-		return (1);
-	}
-	else if (num == 0)
-	{
+	case 0:
 		_putchar('0');
 		return (0);
-	}
-	else if (num < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else
-	{
-		_putchar('-');
-		return ('/');
+	default:
+		if (num > 0)
+		{
+			_putchar('+');
+			return (1);
+		}
+		else if (num < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+		else
+		{
+			_putchar('-');
+			return ('/');
+		}
 	}
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @num: Given number.
+ *
+ * Return: Always 0 (Success)
+ * --------------------------
+ * Write a function that checks for lowercase character.
+ * Prototype: int _islower(int c);
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ * --------------------------
+ * By Youssef Hassane
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		num = num * -1;
+	}
+	return (num);
+}
