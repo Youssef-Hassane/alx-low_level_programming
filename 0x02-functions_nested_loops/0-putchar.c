15d1763c413a433db981a60b1a6863b23f237c2a
youssef-hassane <youssef_hassane@icloud.com>
2023-10-10 16:44:28 +0300

    Answering task number 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * --------------------------
+ * Write a program that prints _putchar, followed by a new line.
+ * The program should return 0
+ *
+ * --------------------------
+ * By Youssef Hassane
+ */
+int main(void)
+{
+	char putChar[] = "_putchar";
+	int num;
+
+	for (num = 0; num <= 7; num++)
+	{
+		putchar(putChar[num]);
+	}
+	putchar('\n');
+	return (0);
+	return (0);
+}
