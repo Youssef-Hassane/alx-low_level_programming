43c58bef79ab33f19105826964f2d5f83ba8fca5
youssef-hassane <youssef_hassane@icloud.com>
2023-10-11 02:49:41 +0300

    Answering task number 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -18,22 +18,22 @@
 
 int main(void)
 {
-	int limit = 1024;
-	int sum = 0;
+	int theLimit = 1024;
+	int theSum = 0;
 
-	/* Iterate through numbers from 0 to limit - 1 */
-	for (int i = 0; i < limit; i++)
+	/* Iterate through numbers from 0 to theLimit - 1 */
+	for (int i = 0; i < theLimit; i++)
 	{
 		/* Check if the current number is a multiple of 3 or 5 */
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			/* If it is, add it to the sum */
-			sum += i;
+			theSum += i;
 		}
 	}
 
 	/* Print the sum of all multiples of 3 or 5 below 1024 */
-	printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
+	printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", theSum);
 
 	return (0);
 }
