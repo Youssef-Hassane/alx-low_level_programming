040531747fdc6fe2ce3998e31a0057589e9dea63
youssef-hassane <youssef_hassane@icloud.com>
2023-10-11 00:47:24 +0300

    Answering task number 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_times_table - Write a function that prints the n times table,
+ * starting with 0.
+ *
+ *
+ * starting with 0.
+ * @n: Given number.
+ *
+ * Return: the last digit of a number
+ * --------------------------
+ * Write a function that prints the n times table,
+ * starting with 0.
+ * --------------------------
+ * By Youssef Hassane
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+		return;
+
+	int i, j, result;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			if (j == 0)
+				_putchar('0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
+					_putchar(' ');
+				if (result < 100)
+					_putchar(' ');
+
+				if (result >= 100)
+					_putchar(result / 100 + '0');
+				if (result >= 10)
+					_putchar((result / 10) % 10 + '0');
+
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
