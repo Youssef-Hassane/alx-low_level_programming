c36ae39390030e096dd5399156e20555efc4c119
youssef-hassane <youssef_hassane@icloud.com>
2023-10-11 01:38:56 +0300

    Answering task number 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,19 +15,25 @@
  * --------------------------
  * By Youssef Hassane
  */
+
 void print_times_table(int givenNumber)
 {
 	int rowNumber, columnNumber, result;
 
+	/* Check if the givenNumber is within the valid range (0 to 15) */
 	if (givenNumber < 0 || givenNumber > 15)
 		return;
 
+	/* Loop through each row */
 	for (rowNumber = 0; rowNumber <= givenNumber; rowNumber++)
 	{
+		/* Loop through each column */
 		for (columnNumber = 0; columnNumber <= givenNumber; columnNumber++)
 		{
+			/* Calculate the product of rowNumber and columnNumber */
 			result = rowNumber * columnNumber;
 
+			/* Print the result with proper formatting */
 			if (columnNumber == 0)
 				_putchar('0');
 			else
@@ -35,11 +41,13 @@ void print_times_table(int givenNumber)
 				_putchar(',');
 				_putchar(' ');
 
+				/* Add leading spaces for proper alignment */
 				if (result < 10)
 					_putchar(' ');
 				if (result < 100)
 					_putchar(' ');
 
+				/* Print the result with proper formatting (hundreds, tens, ones) */
 				if (result >= 100)
 					_putchar(result / 100 + '0');
 				if (result >= 10)
@@ -48,6 +56,7 @@ void print_times_table(int givenNumber)
 				_putchar(result % 10 + '0');
 			}
 		}
+		/* Move to the next row */
 		_putchar('\n');
 	}
 }
