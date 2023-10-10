c5eeab14f20b055beca662926feec30f0141f6ff
youssef-hassane <youssef_hassane@icloud.com>
2023-10-10 19:29:46 +0300

    Answering task number 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,11 +8,7 @@
  *
  * Return: Always 0 (Success)
  * --------------------------
- * Write a function that checks for lowercase character.
- * Prototype: int _islower(int c);
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * WrWrite a function that computes the absolute value of an integer.
  * --------------------------
  * By Youssef Hassane
  */
@@ -24,4 +20,3 @@ int _abs(int num)
 	}
 	return (num);
 }
-k
