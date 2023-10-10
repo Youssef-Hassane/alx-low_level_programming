3d2af37f0672420263947e87fe28b03f1dcd83c9
youssef-hassane <youssef_hassane@icloud.com>
2023-10-10 18:43:04 +0300

    Answering task number 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
+ * print_sign - prints the sign of a number
  *
  * @num: ASCii value.
  *
@@ -10,20 +10,34 @@
  * --------------------------
  * Write a function that checks for lowercase character.
  * Prototype: int _islower(int c);
- * Returns 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
  * --------------------------
  * By Youssef Hassane
  */
 
-int _isalpha(int num)
+int print_sign(int num)
 {
-	if ((num >= 'a' && num <= 'z') || (num >= 'A' && num <= 'Z'))
+
+	if (num > 0)
 	{
+		_putchar('+');
 		return (1);
+	}
+	else if (num == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (num < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-		return (0);
+		_putchar('-');
+		return ('/');
 	}
 }
