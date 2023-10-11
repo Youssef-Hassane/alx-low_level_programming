4d040f9f890113a270979ddd12359af8c316c86a
youssef-hassane <youssef_hassane@icloud.com>
2023-10-11 03:56:45 +0300

    Answering task number 15

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,39 +7,41 @@
  */
 int main(void)
 {
-	void printFirst98Fibonacci(void);
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-	/* Call the function to print the first 98 Fibonacci numbers */
-	printFirst98Fibonacci();
-
-	return (0);
-}
-/**
- * printFirst98Fibonacci - Entry point of the program
- *
- * Return: Always 0
- */
-void printFirst98Fibonacci(void)
-{
-	int i;
-	unsigned long int a = 1, b = 2;
-
-	/* Print the first two Fibonacci numbers without a comma */
-	printf("%lu, %lu\n", a, b);
-
-	for (i = 3; i <= 98; i++)
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
 	{
-		/* Calculate the next Fibonacci number */
-		unsigned long int next = a + b;
-
-		/* Print the comma and space separator, then the next Fibonacci number */
-		printf(", %lu\n", next);
-
-		/* Update 'a' and 'b' for the next iteration */
-		a = b;
-		b = next;
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
 	}
-
-	/* Print a newline character to complete the output */
-	printf("\n");
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
+	return (0);
 }
