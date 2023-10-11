c49cfa33dd8a11fa195943ce97f8671f1fafd7c5
youssef-hassane <youssef_hassane@icloud.com>
2023-10-11 20:48:49 +0300

    Answering task number 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	/* changing the assign value of the variable i from 98 to 0 */
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
