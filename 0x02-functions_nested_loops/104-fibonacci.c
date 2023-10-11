5a701b00271e0a412ffbed77e7bce2d14b0fd746
youssef-hassane <youssef_hassane@icloud.com>
2023-10-11 03:44:29 +0300

    Answering task number 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,7 +24,7 @@ void printFirst98Fibonacci(void)
 	/* Print the first two Fibonacci numbers without a comma */
 	printf("%lu, %lu", a, b);
 
-	for (i = 2; i < 98; i++)
+	for (i = 2; i <= 98; i++)
 	{
 		/* Calculate the next Fibonacci number */
 		unsigned long int next = a + b;
