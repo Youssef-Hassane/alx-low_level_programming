c70c6fc58428fb608ddd99066a382c22e1c34505
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-13 02:42:47 +0300

    Answering task number 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -24,17 +24,19 @@ void print_square(int size)
 {
 	int row, column;
 
-
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-	}
-	for (row = 0; row < size; row++)
-	{
-		for (column = 0; column < (size); column++)
+		for (row = 1; row <= size; row++)
 		{
-			_putchar('#');
+			for (column = 1; column <= size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
