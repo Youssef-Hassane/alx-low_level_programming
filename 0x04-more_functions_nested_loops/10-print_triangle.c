dd3ae881258c987f46f635c76237a5ebe653c0d9
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-13 03:12:05 +0300

    Answering task number 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - Write a program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print Fizz
+ * instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: 0
+ * @size: is the size of the triangle
+ * --------------------------
+ * Write a program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print Fizz
+ * instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * --------------------------
+ * By Youssef Hassane
+ */
+void print_triangle(int size)
+{
+	int theRow = 1,
+	TheHash = 1,
+	theSpace = size - theRow;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (; theRow <= size; theRow++)
+		{
+			for (; theSpace >= 1; theSpace--)
+			{
+				_putchar(' ');
+			}
+			for (; TheHash <= theRow; TheHash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
