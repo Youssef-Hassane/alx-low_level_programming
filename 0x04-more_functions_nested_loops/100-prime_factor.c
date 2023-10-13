91aed5b54eebb1b3f3de6de9248d53008a8db641
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-13 04:01:25 +0300

    Answering task number 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,31 +12,6 @@
 
 int main(void)
 {
-	long number = 612852475143;
-	int inc;
-
-	while (inc++ < number / 2)
-	{
-		if (number % inc == 0)
-		{
-			number /= 2;
-			continue;
-		}
-
-		for (inc = 3; inc < number / 2; inc += 2)
-		{
-			if (number % inc == 0)
-				number /= inc;
-		}
-	}
-	printf("%ld\n", number);
-	return (0);
-}
-
-#include <stdio.h>
-
-int main(void)
-{
 	/* The original number to find the largest prime factor of. */
 	long originalNumber = 612852475143;
 	/* A copy of the original number to work with. */
