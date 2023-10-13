1078921378679a5c641b5bb3af9e6271fcff9752
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-13 03:06:19 +0300

    Answering task number 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,15 +22,26 @@ int main(void)
 	for (number = 1; number <= 100; number++)
 	{
 		if ((number % 3 == 0) && (number % 5 == 0))
+		{
 			printf("FizzBuzz");
+		}
+
 		else if (number % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (number % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
+		{
 			printf("%i", number);
+		}
 		if (number < 100)
+		{
 			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
