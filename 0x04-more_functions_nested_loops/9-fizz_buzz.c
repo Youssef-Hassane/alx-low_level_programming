fa67f0a61d5a9dda28852f9a386044fa0b4984fc
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-13 03:03:44 +0300

    Answering task number 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_square - Write a function that prints a square,
- * followed by a new line.
- * Prototype: void print_square(int size);
- * You can only use _putchar function to print
- * Where size is the size of the square
- * If size is 0 or less, the function should print only a new line
- * Use the character # to print the square
- * @size: the size of the square
+ * main - Write a program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print Fizz
+ * instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
  * --------------------------
- * Write a function that prints a square,
- * followed by a new line.
- * You can only use _putchar function to print
- * Where size is the size of the square
- * If size is 0 or less, the function should print only a new line
- * Use the character # to print the square
+ * Write a program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print Fizz
+ * instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
  * --------------------------
  * By Youssef Hassane
  */
