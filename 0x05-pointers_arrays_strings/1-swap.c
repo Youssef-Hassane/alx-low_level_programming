d25e2520a51de425206f50b0f5473a6a690c6c67
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 00:05:28 +0300

    Answering task number 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,7 @@
 /**
  * swap_int - Write a function that swaps
  * the values of two integers.
- * Return: void
+ * This function does not return a value (void).
  * @firstInt: pointer to the first integer to swap
  * @secondInt: pointer to the second integer to swap
  * --------------------------
@@ -16,8 +16,10 @@
 
 void swap_int(int *firstInt, int *secondInt)
 {
+	/* Store the value pointed to by firstInt in a temporary variable. */
 	int addressOfTheFirstInt = *firstInt;
-
+	/*Set the value pointed to by firstInt to the value pointed to by secondInt.*/
 	*firstInt = *secondInt;
+	/* Set the value pointed to by secondInt to the temporary value. */
 	*secondInt = addressOfTheFirstInt;
 }
