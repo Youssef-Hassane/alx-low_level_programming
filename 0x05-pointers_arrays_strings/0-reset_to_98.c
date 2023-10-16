d0b9e1fda41eedcceb16b88e074222ced77977b9
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-16 23:55:52 +0300

    Answering task number 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Write a function that takes a
+ * pointer to an int as parameter and updates
+ * the value it points to to 98.
+ * Return: void
+ * @n: number of bytes written
+ * --------------------------
+ * Write a function that takes a
+ * pointer to an int as parameter and updates
+ * the value it points to to 98.
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
