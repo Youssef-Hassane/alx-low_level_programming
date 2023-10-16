db94fbef8d5f9b8e5cbe62c87d95711edbb92379
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 02:30:00 +0300

    Answering task number 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,9 @@
  * --------------------------
  * By Youssef Hassane
  */
-void rev_string(char *s)
+void rev_string(char *string)
 {
-	int length = 0;
-
-	/* Calculate the length of the string. */
-	while (s[length] != '\0')
-	{
-		length++;
-	}
+	_strlen(char string);
 
 	/* Initialize two pointers, 'start' and 'end,' */
 	/* to the beginning and end of the string. */
@@ -33,6 +27,7 @@ void rev_string(char *s)
 		/* Swap the characters at the 'start' and 'end' */
 		/* positions using a temporary variable 'temp.' */
 		char temp = s[start];
+
 		s[start] = s[end];
 		s[end] = temp;
 
@@ -40,4 +35,40 @@ void rev_string(char *s)
 		start++;
 		end--;
 	}
+}
+
+/**
+ * _strlen - Write a function that returns the length of a string.
+ * the values of two integers.
+ * This function does not return a value (void).
+ * @string: The given string
+ * Return: The length of the string as an integer.
+ * --------------------------
+ * Write a function that returns the length of a string.
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+int _strlen(char *string)
+{
+	/* Initialize a variable 'length' to store the  */
+	/* length of the string, starting from 0. */
+	int length = 0;
+	/**
+	 * Utilizing a while loop to iterate through the characters of the string.
+	 * while (*string): This is a while loop that continues as
+	 * long as the character pointed to by s is not the null
+	 * terminator ('\0'). The null terminator marks the end of
+	 * a C string, so this loop will continue until the end
+	 * of the string is reached.
+	 */
+	while (*string)
+	{
+		/* For each character, increment the 'length' variable. */
+		length++;
+		/* Move the 'string' pointer to the next character in the string. */
+		string++;
+	}
+	/* Return the final length of the string. */
+	return (length);
 }
