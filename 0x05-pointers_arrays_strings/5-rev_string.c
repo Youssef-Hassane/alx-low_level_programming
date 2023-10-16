a2a8a2532f4a2b9a49d1ebb70814f2cfdc0f8df5
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 02:27:15 +0300

    Answering task number 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,22 +11,33 @@
  * --------------------------
  * By Youssef Hassane
  */
-void rev_string(char *string)
+void rev_string(char *s)
 {
 	int length = 0;
-	int num;
+
 	/* Calculate the length of the string. */
-	while (string[length] != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	num = length - 1;
-	/* Start from the last character and print in reverse order. */
-	for (; num >= 0; num--)
+	/* Initialize two pointers, 'start' and 'end,' */
+	/* to the beginning and end of the string. */
+
+	int start = 0;
+	int end = length - 1;
+
+	/* Use a while loop to reverse the string. */
+	while (start < end)
 	{
-		string[num];
+		/* Swap the characters at the 'start' and 'end' */
+		/* positions using a temporary variable 'temp.' */
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		/* Move 'start' to the right and 'end' to the left to continue swapping. */
+		start++;
+		end--;
 	}
-	/* Print a new line character to create a newline in the output. */
-	_putchar('\n');
 }
