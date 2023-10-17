67c974b67d57ede6c7bdd5de795e535603fb42ed
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 05:04:11 +0300

    Answering task number 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcpy - Write a function that convert a string to an integer.
+* Prototype: int _atoi(char *s);
+
+* The number in the string can be preceded by an infinite number of characters
+* You need to take into account all the - and + signs before the number
+* If there are no numbers in the string, the function must return 0
+* You are not allowed to use long
+* You are not allowed to declare new variables of “type” array
+* You are not allowed to hard-code special values
+* We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code.
+* @string: The input string to be converted to an integer.
+* Return: The integer value extracted from the string.
+* --------------------------
+* Write a function that prints half of a string,
+* followed by a new line.
+* --------------------------
+* By Youssef Hassane
+*/
+int _atoi(char *string)
+{
+	int result = 0;
+
+	int sign = 1;
+
+	/* Skip leading whitespace characters. */
+	while (*string == ' ' || (*string >= 9 && *string <= 13))
+	{
+		string++;
+	}
+
+	/* Handle the sign (positive or negative). */
+	if (*string == '-')
+	{
+		sign = -1;
+		string++;
+	}
+	else if (*string == '+')
+	{
+		string++;
+	}
+
+	/* Process the digits in the string to build the integer result. */
+	while (*string >= '0' && *string <= '9')
+	{
+		int digit = *string - '0';
+
+		/* Check for potential overflow before adding the next digit. */
+		if (result > (INT_MAX - digit) / 10)
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+
+		result = result * 10 + digit;
+		string++;
+	}
+
+	/* Apply the sign and return the result as an integer. */
+	return (sign * result);
+}
