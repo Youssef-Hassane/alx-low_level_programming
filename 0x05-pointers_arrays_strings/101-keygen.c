25cf1f3c9fd40a4c8d1c76b9c1b0ac9c773f8adb
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 05:37:04 +0300

    Answering task number 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Write a function that copies the string pointed
+ * to by src, including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * Return: A pointer to the start of the destination string.
+ * --------------------------
+ * Write a function that prints half of a string,
+ * followed by a new line.
+ * --------------------------
+ * By Youssef Hassane
+ */
+int main(void)
+{
+	srand(time(0));
+	int num = rand();
+
+	printf("%i\n", num);
+	return (0);
+}
