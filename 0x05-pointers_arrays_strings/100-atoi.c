436812babaa3fc1d2c80218fb4b271f710bac0d4
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 05:09:29 +0300

    Answering task number 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,44 +2,42 @@
 #include <stdio.h>
 #include <limits.h>
 /**
- * _atoi - Write a function that convert a string to an integer.
- * Prototype: int _atoi(char *s);
- * The number in the string can be preceded by an infinite number of characters
- * You need to take into account all the - and + signs before the number
- * If there are no numbers in the string, the function must return 0
- * You are not allowed to use long
- * You are not allowed to declare new variables of “type” array
- * You are not allowed to hard-code special values
- * We will use the -fsanitize=signed-integer-overflow
- * gcc flag to compile your code.
- * @string: The input string to be converted to an integer.
- * Return: The integer value extracted from the string.
- * --------------------------
- * Write a function that prints half of a string,
- * followed by a new line.
- * --------------------------
- * By Youssef Hassane
- */
+* _atoi - Write a function that convert a string to an integer.
+* Prototype: int _atoi(char *s);
+
+* The number in the string can be preceded by an infinite number of characters
+* You need to take into account all the - and + signs before the number
+* If there are no numbers in the string, the function must return 0
+* You are not allowed to use long
+* You are not allowed to declare new variables of “type” array
+* You are not allowed to hard-code special values
+* We will use the -fsanitize=signed-integer-overflow
+* gcc flag to compile your code.
+* @string: The input string to be converted to an integer.
+* Return: The integer value extracted from the string.
+* --------------------------
+* Write a function that prints half of a string,
+* followed by a new line.
+* --------------------------
+* By Youssef Hassane
+*/
 int _atoi(char *string)
 {
 	int result = 0;
 
 	int sign = 1;
 
-	/* Skip leading whitespace characters. */
-	while (*string == ' ' || (*string >= 9 && *string <= 13))
+	/* Skip leading non-digit characters. */
+	while (*string != '\0' && !(*string >= '0' && *string <= '9'))
 	{
-		string++;
-	}
-
-	/* Handle the sign (positive or negative). */
-	if (*string == '-')
-	{
-		sign = -1;
-		string++;
-	}
-	else if (*string == '+')
-	{
+		if (*string == '-')
+		{
+			sign = -1;
+		}
+		else if (*string == '+')
+		{
+			sign = 1;
+		}
 		string++;
 	}
 
