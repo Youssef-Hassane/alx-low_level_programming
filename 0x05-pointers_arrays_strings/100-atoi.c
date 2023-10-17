8c64131bcb87052854cc0f73f8b1c8d768986966
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 05:25:38 +0300

    Answering task number 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -29,15 +29,15 @@ int _atoi(char *string)
 	i = minus = result = boolean = 0;
 	minus = -1;
 
-	while (s[i] != '\0')
+	while (string[i] != '\0')
 	{
-		if (s[i] == '-')
+		if (string[i] == '-')
 			minus *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (string[i] >= '0' && string[i] <= '9')
 		{
 			result *= 10;
-			result -= (s[i] - '0');
+			result -= (string[i] - '0');
 			boolean = 1;
 		}
 		else if (boolean == 1)
