371795763aad927cdad50514873120a4a19d9002
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 05:47:49 +0300

    Answering task number 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,32 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Write a function that copies the string pointed
- * to by src, including the terminating null byte (\0),
- * to the buffer pointed to by dest.
- * Return: A pointer to the start of the destination string.
+ * main - Create a program that generates random
+ * valid passwords for the program 101-crackme.
+ * Return: 0 on success
  * --------------------------
- * Write a function that prints half of a string,
- * followed by a new line.
+ * Create a program that generates random
+ * valid passwords for the program 101-crackme.
  * --------------------------
  * By Youssef Hassane
  */
+
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
-
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+	int total_chars = 0, current_char = 0;
+	time_t seed;
+	/* Initialize the random number generator with the current time */
+	srand((unsigned int)time(&seed));
+	while (total_chars < 2772)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
+		/* Generate a random character code between 0 and 127 */
+		current_char = rand() % 128;
+		/* If adding the current character exceeds 2772, exit the loop */
+		if ((total_chars + current_char) > 2772)
+		{
 			break;
-		c = c + r;
-		printf("%c", r);
+		}
+		/* Add the current character to the total count */
+		total_chars = total_chars + current_char;
+		/* Print the character represented by the current code */
+		printf("%c", current_char);
 	}
-	printf("%c\n", (2772 - c));
+	/* Print the remaining character to reach 2772 */
+	printf("%c\n", (2772 - total_chars));
 	return (0);
 }
