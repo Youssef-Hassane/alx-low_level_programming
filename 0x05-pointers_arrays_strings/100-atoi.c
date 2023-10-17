185d683ceb6d50a75767e62828755cba914968e5
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-17 05:33:08 +0300

    Answering task number 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,6 +4,7 @@
 /**
 * _atoi - Write a function that convert a string to an integer.
 * Prototype: int _atoi(char *s);
+
 * The number in the string can be preceded by an infinite number of characters
 * You need to take into account all the - and + signs before the number
 * If there are no numbers in the string, the function must return 0
@@ -20,3 +21,42 @@
 * --------------------------
 * By Youssef Hassane
 */
+int _atoi(char *string)
+{
+	/* A flag to indicate if we've encountered a digit */
+	short digit_found;
+
+	/* Variables for iteration, sign, and the final result */
+	int num, sign, result;
+
+	num = sign = result = digit_found = 0;
+	/* Initialize 'sign' to -1 to handle negative numbers */
+	sign = -1;
+
+	while (*string != '\0')
+	{
+		if (*string == '-')
+		{
+			/* Toggle the sign if a '-' is encountered */
+			sign *= -1;
+		}
+
+		if (*string >= '0' && *string <= '9')
+		{
+			/* Multiply the result by 10 to shift digits left */
+			result *= 10;
+			/* Subtract the ASCII value to get the digit */
+			result -= (*string - '0');
+			/* Set the flag to indicate that a digit is found */
+			digit_found = 1;
+		}
+		else if (digit_found == 1)
+			/* If we encountered a digit and then a non-digit, break the loop */
+			break;
+
+		string++;
+	}
+	/* Apply the sign to the final result */
+	result *= sign;
+	return (result);
+}
