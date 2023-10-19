f056e7d047d3f63b7adfdad4eff242ca973a9e92
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-19 20:53:28 +0300

    Answering task number 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - Write a function that compares two strings.
+ * Prototype: int _strcmp(char *s1, char *s2);
+ * Your function should work exactly like strcmp
+ * --------------------------
+ * @firstString: the first string to compare
+ * @secondString: the second string to compare
+ * Return: the starting address of the concatenated string
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+int _strcmp(char *firstString, char *secondString)
+{
+	/* the return value */
+	int output;
+	int zeroVarForReturn = 0;
+
+	/* looping through both the first and second strings */
+	for (; firstString || secondString; firstString++, secondString++)
+	{
+		/* check if the first string is greater than the second string */
+		/* and if so, return the substring comparison */
+		if (firstString > secondString)
+		{
+			output = firstString - secondString;
+			return (output);
+		}
+		/* check if the first string is less than the second string */
+		/* and if so, return the substring corresponding */
+		else if (secondString > firstString)
+		{
+			output = firstString - secondString;
+			return (output);
+		}
+		else
+		{
+			return (zeroVarForReturn);
+		}
+	}
+	return (zeroVarForReturn);
+}
