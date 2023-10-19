427b59d270880d8ef0c0afe4d3dfa5b8f3af25c8
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-20 01:19:11 +0300

    Answering task number 4

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,6 @@ char *_strcat(char *destination, char *source);
 char *_strncat(char *destination, const char *source, int n);
 char *_strncpy(char *destination, const char *source, int n);
 int _strcmp(char *firstString, char *secondString);
-void reverse_array(int *a, int n);
+void reverse_array(int *array, int numberOfElementsInArray);
 
 #endif
