001cdd8502ceda40a3eb12f3113f0cc4340d7655
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-20 03:45:42 +0300

    Answering task number 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,44 +2,44 @@
 #include <stdio.h>
 
 /**
- * cap_string - Write a function that capitalizes all words of a string.
- * --------------------------
- * @string: The array of characters to be converted to uppercase
- * Return: A pointer to the modified string
- * --------------------------
- * By Youssef Hassane
- */
-
+* cap_string - Capitalizes the first letter of each word in a string.
+* @string: The string to be modified.
+* Return: A pointer to the modified string.
+*/
 char *cap_string(char *string)
 {
 	int index = 0;
 
-	/* Loop through the string until the null terminator is reached. */
+	int capitalize_next = 1;
+
 	while (string[index] != '\0')
 	{
-		/* Capitalize the first letter of the string if it's a lowercase letter. */
-		if (string[0] >= 97 && string[0] <= 122)
+		if ((string[index] >= 'a' && string[index] <= 'z') ||
+		 (string[index] >= 'A' && string[index] <= 'Z'))
 		{
-			string[0] = string[0] - 32;
-		}
-
-		/* Check for various separator characters like space, tab, etc. */
-		if (string[index] == ' ' || string[index] == '\t' || string[index] == '\n' ||
-		    string[index] == ',' || string[index] == ';' || string[index] == '.' ||
-		    string[index] == '.' || string[index] == '!' || string[index] == '?' ||
-		    string[index] == '"' || string[index] == '(' || string[index] == ')' ||
-		    string[index] == '{' || string[index] == '}')
-		{
-			/* If the character after the separator is a lowercase letter */
-			/* capitalize it. */
-			if (string[index + 1] >= 97 && string[index + 1] <= 122)
+			if (capitalize_next)
 			{
-				string[index + 1] = string[index + 1] - 32;
+				if (string[index] >= 'a' && string[index] <= 'z')
+				{
+					string[index] -= 32;
+				}
+				capitalize_next = 0;
+			}
+			else
+			{
+				if (string[index] >= 'A' && string[index] <= 'Z')
+				{
+					string[index] += 32;
+				}
 			}
 		}
+		else
+		{
+			capitalize_next = 1;
+		}
 
-		index++; /* Move to the next character in the string. */
+		index++;
 	}
-	/* Return a pointer to the modified string. */
+
 	return (string);
 }
