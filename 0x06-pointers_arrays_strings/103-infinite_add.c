76496f357189615af1deb39c69f304fd95a12223
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-20 05:32:13 +0300

    Answering task number 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,26 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * infinite_add - Write a function that adds two numbers.
- * --------------------------------------------------------------
  * @num1: the first number
  * @num2: the second number
- * @result: the result of the function call 
+ * @result: the result of the function call
  * @result_size: the size of the result buffer in bytes
- * Prototype: char *infinite_add(char *n1, char *n2, char *r, int size_r);
- * Where n1 and n2 are the two numbers
- * r is the buffer that the function will use to store the result
- * size_r is the buffer size
- * The function returns a pointer to the result
- * You can assume that you will always get positive numbers, or 0
- * You can assume that there will be only digits in the strings n1 and n2
- * n1 and n2 will never be empty
- * If the result can not be stored in r the function must return 0
- * --------------------------------------------------------------
- * By Youssef Hassane
+ * Return: the result of the function call
  */
-
 char *infinite_add(char *num1, char *num2, char *result, int result_size)
 {
 	int index_num1, index_num2, index_result, carry, sum, digit;
@@ -32,11 +19,9 @@ char *infinite_add(char *num1, char *num2, char *result, int result_size)
 	if (index_num1 > result_size || index_num2 > result_size)
 		return (0);
 	carry = 0;
-
 	index_num1 -= 1;
 	index_num2 -= 1;
 	index_result = 0;
-
 	while (index_result < result_size - 1)
 	{
 		sum = carry;
@@ -45,26 +30,18 @@ char *infinite_add(char *num1, char *num2, char *result, int result_size)
 		if (index_num2 >= 0)
 			sum += num2[index_num2] - '0';
 		if (index_num1 < 0 && index_num2 < 0 && sum == 0)
-		{
 			break;
-		}
 		carry = sum / 10;
 		digit = sum % 10 + '0';
 		result[index_result] = digit;
-
-		// Update loop variables
 		index_num1--;
 		index_num2--;
 		index_result++;
 	}
-
 	result[index_result] = '\0';
-
 	if (index_num1 >= 0 || index_num2 >= 0 || carry)
 		return (0);
-
 	index_result--;
-
 	while (l < k)
 	{
 		digit = result[index_result];
@@ -73,6 +50,5 @@ char *infinite_add(char *num1, char *num2, char *result, int result_size)
 		index_result--;
 		l++;
 	}
-
 	return (result);
 }
