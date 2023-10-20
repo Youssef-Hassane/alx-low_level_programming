ff30503829a5d5de8bb7968e7370b12e7296b21a
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-20 06:37:58 +0300

    Answering task number 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,80 +2,49 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Write a function that prints a buffer.
- * ----------------------------------------------------------------
- * Prototype: void print_buffer(char *b, int size);
- * @buffer: The buffer to print.
- * @size: The size of the buffer in bytes.
- * ----------------------------------------------------------------
- * by Youssef Hassane
+ * print_buffer - a function that prints a buffer.
+ * @b: the buffer
+ * @size: size of it
+ * Return: void.
  */
-void print_buffer(char *buffer, int size)
+
+void print_buffer(char *b, int size)
 {
-	int offset;
-	int byteIndex;
-	/* Check if the size is less than or equal to 0 */
+	int offset, i, j;
+
+	offset = 0;
+
 	if (size <= 0)
 	{
-		/* Print a newline and exit the function */
 		printf("\n");
 		return;
 	}
-	/* Loop through the buffer with a 10-byte offset */
-	for (offset = 0; offset < size; offset += 10)
+	while (offset < size)
 	{
-		/* Print the offset in hexadecimal format with leading zeros */
+		i = size - offset < 10 ? size - offset : 10;
 		printf("%08x: ", offset);
-		/* Loop through 10 bytes or less, depending on the remaining buffer size */
-		for (byteIndex = 0; byteIndex < 10; byteIndex++)
+		for (j = 0; j < 10; j++)
 		{
-			if (offset + byteIndex < size)
-			{
-				/* Print the byte in hexadecimal format */
-				printf("%02x", (unsigned char)buffer[offset + byteIndex]);
-			}
+			if (j < i)
+				printf("%02x", *(b + offset + j));
 			else
-			{
-				/* Print two spaces for empty bytes */
 				printf("  ");
-			}
-			if (byteIndex % 2 == 1)
+			if (j % 2)
 			{
-				/* Print a space every two bytes */
-				printf(" ");
-			}
-			if (byteIndex == 9)
-			{
-				/* Print an extra space after the 10th byte */
 				printf(" ");
 			}
 		}
-		/*Loop through the same 10 bytes or less to print their ASCIIrepresentation*/
-		for (byteIndex = 0; byteIndex < 10; byteIndex++)
+		for (j = 0; j < i; j++)
 		{
-			if (offset + byteIndex < size)
+			int c = *(b + offset + j);
+
+			if (c < 32 || c > 132)
 			{
-				if (buffer[offset + byteIndex] >= ' ' && buffer[offset + byteIndex] <= '~')
-				{
-					/* If the byte is a printable character, print it */
-					printf("%c", buffer[offset + byteIndex]);
-				}
-				else
-				{
-					/* If the byte is non-printable, print a dot */
-					printf(".");
-				}
+				c = '.';
 			}
-			else
-			{
-				/* Print a space for empty bytes */
-				printf(" ");
-			}
-			if (byteIndex == 9)
-			{
-				/* Print a newline after printing 10 bytes */
-				printf("\n");
-			}
+			printf("%c", c);
 		}
+		printf("\n");
+		offset += 10;
 	}
 }
