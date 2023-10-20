cd675223dbacc00fcf86549d7f44b4c53adf97ee
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-20 03:41:37 +0300

    Answering task number 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,34 +12,34 @@
 
 char *cap_string(char *string)
 {
-	char *output = string;
-	
-	/* Looping through the string until reaching the null terminator */
-	while (*string != '\0')
+	int index = 0;
+
+	/* Loop through the string until the null terminator is reached. */
+	while (string[index] != '\0')
 	{
 		/* Capitalize the first letter of the string if it's a lowercase letter. */
-		if (*string >= 97 && *string <= 122)
+		if (string[0] >= 97 && string[0] <= 122)
 		{
-			*string = *string - 32;
+			string[0] = string[0] - 32;
 		}
 
 		/* Check for various separator characters like space, tab, etc. */
-		if (*string == ' ' || *string == '\t' || *string == '\n' ||
-		    *string == ',' || *string == ';' || *string == '.' ||
-		    *string == '.' || *string == '!' || *string == '?' ||
-		    *string == '"' || *string == '(' || *string == ')' ||
-		    *string == '{' || *string == '}')
+		if (string[index] == ' ' || string[index] == '\t' || string[index] == '\n' ||
+		    string[index] == ',' || string[index] == ';' || string[index] == '.' ||
+		    string[index] == '.' || string[index] == '!' || string[index] == '?' ||
+		    string[index] == '"' || string[index] == '(' || string[index] == ')' ||
+		    string[index] == '{' || string[index] == '}')
 		{
 			/* If the character after the separator is a lowercase letter */
-			/* capitalize it*/
-			if (*string >= 97 && *string <= 122)
+			/* capitalize it. */
+			if (string[index + 1] >= 97 && string[index + 1] <= 122)
 			{
-				*string = *string - 32;
+				string[index + 1] = string[index + 1] - 32;
 			}
 		}
-		/* Move to the next character in the string. */
-		string++;
+
+		index++; /* Move to the next character in the string. */
 	}
 	/* Return a pointer to the modified string. */
-	return (output);
+	return (string);
 }
