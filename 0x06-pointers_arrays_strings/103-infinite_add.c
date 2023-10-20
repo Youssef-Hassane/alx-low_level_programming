4dd5ba694073dab899cffdbcd69f33aec8379bfb
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-20 05:45:17 +0300

    Answering task number 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,37 +12,37 @@ char *infinite_add(char *num1, char *num2, char *result, int result_size)
 {
 	int index_num1, index_num2, index_result, carry, sum, digit;
 
-	for (index_num1 = 0; n1[index_num1]; index_num1++)
+	for (index_num1 = 0; num1[index_num1]; index_num1++)
 		;
-	for (index_num2 = 0; n2[index_num2]; index_num2++)
+	for (index_num2 = 0; num2[index_num2]; index_num2++)
 		;
-	if (index_num1 > size_r || index_num2 > size_r)
+	if (index_num1 > result_size || index_num2 > result_size)
 		return (0);
 	sum = 0;
 	for (index_num1 -= 1, index_num2 -= 1, index_result = 0;
-	index_result < size_r - 1; index_num1--, index_num2--, index_result++)
+	index_result < result_size - 1; index_num1--, index_num2--, index_result++)
 	{
 		digit = sum;
 		if (index_num1 >= 0)
-			digit += n1[index_num1] - '0';
+			digit += num1[index_num1] - '0';
 		if (index_num2 >= 0)
-			digit += n2[index_num2] - '0';
+			digit += num2[index_num2] - '0';
 		if (index_num1 < 0 && index_num2 < 0 && digit == 0)
 		{
 			break;
 		}
 		sum = digit / 10;
-		r[index_result] = digit % 10 + '0';
+		result[index_result] = digit % 10 + '0';
 	}
-	r[index_result] = '\0';
+	result[index_result] = '\0';
 	if (index_num1 >= 0 || index_num2 >= 0 || m)
 		return (0);
 	for (index_result -= 1, carry = 0; carry < index_result; index_result--,
 	carry++)
 	{
-		sum = r[index_result];
-		r[index_result] = r[carry];
-		r[carry] = sum;
+		sum = result[index_result];
+		result[index_result] = result[carry];
+		result[carry] = sum;
 	}
-	return (r);
+	return (result);
 }
