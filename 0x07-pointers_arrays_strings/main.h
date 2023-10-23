b2dbe47fbff8e68c2cf25253028705b00212b174
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-23 21:21:52 +0300

    Answering task number 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_memset(char *memoryToBeFilled,
 		  char byteValueToFill,
 		  unsigned int numberOfBytesToBeFilled);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
 
 #endif
