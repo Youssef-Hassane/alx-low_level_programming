bb119ca09d2c0479b606ebd5cdc97c9ce270a3ef
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-24 04:21:49 +0300

    Answering task number 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - Write a function that prints the chessboard.
+ * --------------------------
+ * Prototype: void print_chessboard(char (*a)[8]);
+ * The function takes a pointer to an 8x8 character array as input
+ * --------------------------
+ * @a: a pointer to the memory area representing the chessboard
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	/* Declare variables for the chessboard*/
+	int row, column;
+	/* Loop through each row of the chessboard */
+	for (int row = 0; row < 8; row++)
+	{
+		/* Loop through each column of the chessboard */
+		for (int column = 0; column < 8; column++)
+		{
+			/* Print the character at position (row, column)*/
+			_putchar(a[row][column]);
+		}
+		/* Move to a new line after each row is printed */
+		_putchar('\n');
+	}
+}
