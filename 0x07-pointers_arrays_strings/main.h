72b24928cda7dfff99821e60a4358d9a6ce2d4c4
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-24 04:14:15 +0300

    Answering task number 5

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_memcpy(char *destination, char *source, unsigned int numberOfBytes);
 char *_strchr(char *string, char character);
 unsigned int _strspn(char *string, char *accept);
 char *_strpbrk(char *string, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
