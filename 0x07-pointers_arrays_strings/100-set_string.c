3016db84c34127560b146da4ab4743b07d370845
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-24 05:08:45 +0300

    Answering task number 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - Write a function that sets the
+ * value of a pointer to a char.
+ * --------------------------
+ * Prototype: void set_string(char **s, char *to);
+ * --------------------------
+ * @string: A pointer to a pointer to char.
+ * @to: The string to set the pointer to.
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+void set_string(char **string, char *to)
+{
+	*string = to;
+}
