459ace372dbb6ba981ea77132d456369520b55ce
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-25 18:08:07 +0300

    Answering task number 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - Write a function that
+ * returns the factorial of a given number.
+ * Return: Always 0 (Success)
+ * --------------------------
+ * Prototype: int factorial(int n);
+ * @number: The number to calculate the factorial of
+ * If n is lower than 0, the function
+ * should return -1 to indicate an error
+ * Factorial of 0 is 1
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+int factorial(int number)
+{
+	/* a variable to store the result */
+	int result;
+	/* if number is lower than 0, return -1 */
+	if (number < 0)
+	{
+		return (-1);
+	}
+	/* if number is 0, return 1 */
+	else if (number == 0)
+	{
+		return (1);
+	}
+	/* if number is greater than 0, calculate the factorial */
+	else
+	{
+		/* call the function again with the rest of the number */
+		result = number * factorial(number - 1);
+	}
+	/* return the result */
+	return (result);
+}
