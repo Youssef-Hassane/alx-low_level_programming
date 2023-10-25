8c2e164ff204ea78b4091eb207077a3475eb2192
Youssef Hassane <Youssef_hassane@icloud.com>
2023-10-25 20:50:01 +0300

    Answering task number 5

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,6 @@ void _print_rev_recursion(char *string);
 int _strlen_recursion(char *string);
 int factorial(int number);
 int _pow_recursion(int base, int power);
+int _sqrt_recursion(int n);
 
 #endif
