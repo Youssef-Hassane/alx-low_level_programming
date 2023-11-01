fe1419efb0236f1546750d85a2b347575f50bbd8
youssef-hassane <youssef_hassane@icloud.com>
2023-11-01 03:02:51 +0000

    Answering task number 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,8 +11,7 @@
  */
 
 char **strtow(char *str)
-{
-	int total_words = 0, b = 0, c = 0, length = 0;
+{int total_words = 0, b = 0, c = 0, length = 0;
 	char **words, *found_word;
 
 	if (str == 0 || *str == 0)
@@ -23,7 +22,7 @@ char **strtow(char *str)
 	words = malloc((total_words + 1) * sizeof(char *));
 	if (words == 0)
 		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
+	for (; *str != '\0' && b < total_words;)
 	{
 		if (*str == ' ')
 			str++;
@@ -37,18 +36,19 @@ char **strtow(char *str)
 			}
 			words[b] = malloc((length + 1) * sizeof(char));
 			if (words[b] == 0)
-			{
-				free_string_array(words);
+			{free_string_array(words);
 				return (NULL);
 			}
 			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
+			{words[b][c] = *found_word;
 				found_word++;
 				c++;
 			}
 			words[b][c] = '\0';
-			b++; c = 0; length = 0; str++;
+			b++;
+			c = 0;
+			length = 0;
+			str++;
 		}
 	}
 	return (words);
