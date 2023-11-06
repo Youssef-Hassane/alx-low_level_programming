5d1e107abf9f684eefe049c7a6d7f5ca79655c49
youssef-hassane <youssef_hassane@icloud.com>
2023-11-06 06:27:15 +0000

    Answering task number 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,10 +18,14 @@
 
 void print_dog(struct dog *theDog)
 {
+	/* Check if theDog pointer is not NULL */
 	if (theDog != ((void *)0))
 	{
+		/* Print the dog's name or "(nil)" if it's NULL */
 		printf("Name: %s\n", ((*theDog).name != NULL) ? (*theDog).name : "(nil)");
+		/* Print the dog's age with 6 decimal places */
 		printf("Age: %.6f\n", (*theDog).age);
+		/* Print the owner's name or "(nil)" if it's NULL */
 		printf("Owner: %s\n", ((*theDog).owner != NULL) ? (*theDog).owner : "(nil)");
 	}
 }
