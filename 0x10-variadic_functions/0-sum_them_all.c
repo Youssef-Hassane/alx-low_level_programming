6c16fc78896fd7c56b2cad2b11c75fe8e1e338b7
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-10 00:39:29 +0200

    Answering task number 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - Write a function that returns the sum of all its parameters.
+ * If n == 0, return 0
+ * Return: the sum of the given integers
+ * --------------------------
+ * Prototype: int sum_them_all(const unsigned int n, ...);
+ * @numberOfArguments: the number of arguments
+ * --------------------------
+ * By Youssef Hassane
+ */
+
 int sum_them_all(const unsigned int numberOfArguments, ...)
 {
 	/* Declare a variable called args of type va_list */
 	va_list args;
 	/* Declare an integer variable called index */
 	/* that will be utilized to iterate through the arguments */
-	int index;
+	unsigned int index = 0;
 	/* Declare an integer variable called sum */
 	/* that will be utilized to store the sum of the given arguments */
 	int theSumOfGivenArguments = 0;
@@ -21,10 +32,12 @@ int sum_them_all(const unsigned int numberOfArguments, ...)
 	/* Initialize the variable args with the address of the first argument */
 	va_start(args, numberOfArguments);
 	/* Iterate through the arguments */
-	for (index = 0; index < numberOfArguments; index++)
+	for (; index < numberOfArguments; index++)
 	{
+		/* Adding the current argument to the sum */
 		theSumOfGivenArguments += va_arg(args, int);
 	}
+	/* Closing the variable args */
 	va_end(args);
 
 	return (theSumOfGivenArguments);
