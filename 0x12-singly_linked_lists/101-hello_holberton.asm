2a4c2f03f4810a51e2c3ca676b3d3e15a3d16977
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-17 04:17:20 +0200

    Answering task number 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,24 @@
-section .data
-    hello_format db "Hello, Holberton", 0
+; External declaration for printf
+extern printf
 
+; Data section
+section .data
+    msg: db "Hello, Holberton", 0  ; Null-terminated string
+    fmt: db "%s", 10, 0            ; Format string with newline character and null terminator
+
+; Text section
 section .text
-    global main
-    extern printf
+    global main                    ; Entry point for the program
 
 main:
-    ; Call printf with the format string
-    mov rdi, hello_format
-    call printf
+    push rbp                       ; Save the base pointer
 
-    ; Exit the program
-    mov eax, 0
-    ret
+    mov rdi, fmt                   ; Set the format string address to rdi
+    mov rsi, msg                   ; Set the message address to rsi
+    mov rax, 0                     ; Set rax to 0 (indicating the number of floating-point arguments)
+    call printf                    ; Call printf with the specified format and message
+
+    pop rbp                        ; Restore the base pointer
+
+    mov rax, 0                     ; Set rax to 0 (indicating successful program execution)
+    ret                            ; Return from the main function
