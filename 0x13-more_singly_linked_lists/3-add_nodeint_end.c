591a4b01dd4bebeb3a44bbf3153d80852683350e
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-21 19:46:49 +0200

    Answering task number 2

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Write a function that adds a new
+ * node at the end of a listint_t list.
+ * Return: the address of the new element, or NULL if it failed
+ * --------------------------
+ * Prototype: listint_t *add_nodeint_end(listint_t **head, const int n);
+ * --------------------------
+ * @head: The listint_t list to be printed
+ * @integerToBeAdded: The integer to be added
+ * --------------------------
+ * By Youssef Hassane
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int integerToBeAdded)
+{
+	/* Initialize a new node */
+	listint_t *newNode;
+	listint_t *last = *head;
+	/* Allocate memory for the new node */
+	newNode = malloc(sizeof(listint_t));
+	/* Check if malloc was successful */
+	if (newNode == NULL)
+		return (NULL);
+	/* Set the value of the new node */
+	newNode->n = integerToBeAdded;
+	/* Set the next pointer of the new node to NULL */
+	newNode->next = NULL;
+	/* Check if the list is empty */
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	/* Check if the list is not empty */
+	while (last->next)
+	{
+		/* Move to the next node */
+		last = last->next;
+	}
+	/* Set the next pointer of the last node to the new node */
+	last->next = newNode;
+	/* Return the address of the new node */
+	return (newNode);
+}
