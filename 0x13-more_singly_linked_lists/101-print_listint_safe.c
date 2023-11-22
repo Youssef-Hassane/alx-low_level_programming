e914926b71e18f5bd1e2570675c2c2f317b0f975
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-22 02:50:55 +0200

    Answering task number 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,53 +17,22 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *slow, *fast, *temp;
-    size_t i, num_nodes = 0;
+	size_t x;
+	size_t counter = 0;
+	const listint_t *node_list[1000];
 
-    if (!head)
-        exit(98);
-
-    slow = head;
-    fast = head;
-
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        /* Check if a loop exists */
-        if (slow == fast)
-        {
-            temp = head;
-            while (temp)
-            {
-                /* print the nodes before starting the loop */
-                for (i = 0; i < num_nodes; ++i)
-                {
-                    printf("[%p] %d\n", (void *)temp, temp->n);
-                    temp = temp->next;
-                }
-
-                /* print the loop */
-                do {
-                    printf("[%p] %d\n", (void *)temp, temp->n);
-                    temp = temp->next;
-                } while (temp != slow);
-
-                printf("-> [%p] %d\n", (void *)temp, temp->n);
-                return (num_nodes);
-            }
-        }
-
-        num_nodes++;
-    }
-
-    temp = head;
-    while (temp)
-    {
-        printf("[%p] %d\n", (void *)temp, temp->n);
-        temp = temp->next;
-    }
-
-    return (num_nodes);
+	while (head)
+	{
+		for (x = 0; x <= counter; x++)
+			if (node_list[x] == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				return (counter);
+			}
+		node_list[counter] = head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		counter++;
+	}
+	return (counter);
 }
