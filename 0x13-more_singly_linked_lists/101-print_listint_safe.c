b752d3899000726a9f7d7a027e021630f3081e58
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-22 02:53:03 +0200

    Answering task number 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,31 +8,31 @@
  * Return: The number of nodes in the list
  * exit program with status 98 on failure
  * --------------------------
- * Prototype: print_listint_safe(const listint_t *head);
+ * Prototype: print_listint_safe(const listint_t *headNode);
  * --------------------------
- * @head:  pointer to the head of the list.
+ * @headNode:  pointer to the head of the list.
  * --------------------------
  * By Youssef Hassane
  */
 
-size_t print_listint_safe(const listint_t *head)
+size_t print_listint_safe(const listint_t *headNode)
 {
-	size_t x;
-	size_t counter = 0;
-	const listint_t *node_list[1000];
+	size_t idx;
+	size_t nodeCounter = 0;
+	const listint_t *nodeList[1000];
 
-	while (head)
+	while (headNode)
 	{
-		for (x = 0; x <= counter; x++)
-			if (node_list[x] == head)
+		for (idx = 0; idx <= nodeCounter; idx++)
+			if (nodeList[idx] == headNode)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				return (counter);
+				printf("-> [%p] %d\n", (void *)headNode, headNode->n);
+				return (nodeCounter);
 			}
-		node_list[counter] = head;
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		counter++;
+		nodeList[nodeCounter] = headNode;
+		printf("[%p] %d\n", (void *)headNode, headNode->n);
+		headNode = headNode->next;
+		nodeCounter++;
 	}
-	return (counter);
+	return (nodeCounter);
 }
