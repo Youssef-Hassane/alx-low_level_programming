03912c32af4b36eb4cf92759bc679b5555426aea
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-24 01:26:12 +0200

    Answering task number 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "Logic/Binary_To_Uint_Internal_Logic.c"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * Return: the converted number, or 0 if there is one or more chars
+ * in the string b that is not 0 or 1, or if b is NULL
+ * --------------------------
+ * Prototype: size_t listint_len(const listint_t *h);
+ * --------------------------
+ * @theGivenBinaryNumber: pointer to a string of 0 and 1 chars
+ * --------------------------
+ * By Youssef Hassane
+ */
+unsigned int binary_to_uint(const char *theGivenBinaryNumber)
+{
+	/* Check if the given string is NULL */
+	/* Return 0 if the input is NULL */
+	return ((theGivenBinaryNumber == theNull)
+			? 0
+			: Binary_To_Uint_Internal_Logic(theGivenBinaryNumber));
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define theNull ((void *)0)
+
+unsigned int binary_to_uint(const char *theGivenBinaryNumber);
+
+#endif
