ac51aadec816bb25d39a6f4bada84fe0b5b1924c
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-24 01:51:24 +0200

    Answering task number 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,8 @@
 
 #define theNull ((void *)0)
 
+int _putchar(char character);
 unsigned int binary_to_uint(const char *theGivenBinaryNumber);
+void print_binary(unsigned long int theNumberToPrintInBinary);
 
 #endif
