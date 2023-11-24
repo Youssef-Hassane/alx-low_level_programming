7015974a3c3bd75f611544ac9ce1b209e8fa6687
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-24 02:08:17 +0200

    Answering task number 2

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@
 int _putchar(char character);
 unsigned int binary_to_uint(const char *theGivenBinaryNumber);
 void print_binary(unsigned long int theNumberToPrintInBinary);
+int get_bit(unsigned long int number, unsigned int index);
 
 #endif
