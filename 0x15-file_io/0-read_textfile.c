f159d43721cac41a165ee24dfeb0c5a4b9156ab0
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-27 23:56:55 +0200

    Answering task number 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,33 +5,29 @@
 #include <fcntl.h>
 
 /**
-* read_textfile - Write a function that reads a text
-* file and prints it to the POSIX standard output
-* Return: the actual number of letters it could read and print
-* --------------------------
-* Prototype: ssize_t read_textfile(const char *filename, size_t letters);
-* if the file can not be opened or read, return 0
-* if filename is NULL return 0
-* if write fails or does not write the expected amount of bytes, return 0
-* --------------------------
-* @filename: pointer to a string of 0 and 1 chars
-* @letters: the number of letters it should read and print
-* --------------------------
-* By Youssef Hassane
-*/
+ * read_textfile - Write a function that reads a text
+ * file and prints it to the POSIX standard output
+ * Return: the actual number of letters it could read and print
+ * --------------------------
+ * Prototype: ssize_t read_textfile(const char *filename, size_t letters);
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ * --------------------------
+ * @filename: pointer to a string of 0 and 1 chars
+ * @letters: the number of letters it should read and print
+ * --------------------------
+ * By Youssef Hassane
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-
+	int fd = open(filename, O_RDONLY);
 	char *buffer = malloc(letters + 1);
-
 	ssize_t total = 0, n;
 
 	if (filename == NULL)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
