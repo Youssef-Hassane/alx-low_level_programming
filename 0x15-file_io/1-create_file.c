1914ff54dabbb8d16fd2378be200a1af4921d83b
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-28 03:48:33 +0200

    Answering task number 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,40 +8,43 @@
 /**
  * create_file - Creates a file with specified
  * permissions and writes content to it.
+ * Return: 1 on success, -1 on failure.
+ * --------------------------
  * @filename: Name of the file to create.
  * @text_content: NULL-terminated string to write to the file.
- * Return: 1 on success, -1 on failure.
+ * --------------------------
+ * By Youssef Hassane
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_result, close_result;
-
+	int theFile;
+	int writerTheResult;
+	int closeTheResult;
+	/* Check if filename is valid */
 	if (filename == NULL)
 		return (-1);
-
 	/* Open the file with read and write permissions */
 	/* creating it if it doesn't exist */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	if (fd == -1)
+	theFile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	/* Check if theFile is valid */
+	if (theFile == -1)
 		return (-1);
-
 	/* Write the content to the file if provided */
 	if (text_content != NULL)
 	{
-		write_result = write(fd, text_content, strlen(text_content));
-
-		if (write_result == -1)
+		writerTheResult = write(theFile, text_content, strlen(text_content));
+		/* Check if writerTheResult is valid */
+		if (writerTheResult == -1)
 		{
-			close(fd);
+			close(theFile);
 			return (-1);
 		}
 	}
-
 	/* Close the file descriptor */
-	close_result = close(fd);
-
-	if (close_result == -1)
+	closeTheResult = close(theFile);
+	/* Check if closeTheResult is valid */
+	if (closeTheResult == -1)
 		return (-1);
 
 	return (1);
