dc384223cdc25a90e01b031e610e8eeeff9bbd2b
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-28 04:29:53 +0200

    Answering task number 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,89 +1,60 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-/**
- * copy - copy files
- * @fd_from: first file discreptor
- * @fd_to: second file discreptor
- * @f_from: first file name
- * @f_to: second file name
- */
-void copy(int fd_from, int fd_to, char *f_from, char *f_to)
+#define BUFFER_SIZE 1024
+
+void print_error(int code, const char *message, const char *arg)
 {
-	int printed, flag;
-	char buff[1024];
-
-	printed = read(fd_from, buff, 1024);
-	if (printed == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", f_from);
-		exit(98);
-	}
-	while (printed > 0)
-	{
-		flag = write(fd_to, buff, printed);
-		if (flag != printed)
-		{
-			dprintf(2, "Error: Can't write to %s\n", f_to);
-			exit(99);
-		}
-		printed = read(fd_from, buff, 1024);
-		if (printed == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", f_from);
-			exit(98);
-		}
-	}
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
 }
-/**
- * main - copies from one file to another
- * @argc: num of arguments
- * @argv: value of arguments
- * Return: 0 on success
- */
-int main(int argc, char **argv)
-{
-	int fd_from, fd_to, flag;
 
+int main(int argc, char *argv[])
+{
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		print_error(97, "Usage: %s file_from file_to\n", argv[0]);
 	}
+
+	int fd_from, fd_to;
+	ssize_t read_bytes, write_bytes;
+	char buffer[BUFFER_SIZE];
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 	}
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+	| S_IWUSR | S_IRGRP | S_IROTH);
 	if (fd_to == -1)
 	{
+		print_error(99, "Error: Can't write to file %s\n", argv[2]);
+	}
 
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		flag = close(fd_from);
-		if (flag == -1)
+	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes != read_bytes)
 		{
-			dprintf(2, "Error: Can't close fd %d\n", fd_from);
-			exit(100);
+			print_error(99, "Error: Can't write to file %s\n", argv[2]);
 		}
-		exit(99);
 	}
-	copy(fd_from, fd_to, argv[1], argv[2]);
-	flag = close(fd_to);
-	if (flag == -1)
+
+	if (read_bytes == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 	}
-	flag = close(fd_from);
-	if (flag == -1)
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
+		print_error(100, "Error: Can't close fd %d\n", fd_from > fd_to ? fd_from : fd_to);
 	}
-	return (0);
+
+	return 0;
 }
