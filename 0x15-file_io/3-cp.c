cf03448ca8477062138831ad3d7cc3e98ecc5093
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-28 02:43:12 +0200

    Answering task number 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -62,7 +62,6 @@ int main(int argc, char **argv)
 	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (fd_to == -1)
 	{
-		fd_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
 		if (fd_to == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
