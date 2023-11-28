83e8a27f804bddf0c4d7bc04dcf88be6182e8b81
Youssef Hassane <Youssef_hassane@icloud.com>
2023-11-28 05:08:13 +0200

    Answering task number 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,66 +5,68 @@
 #include <unistd.h>
 
 /**
- * _strncmp - compare two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the max number of bytes to compare
+ * customStrncmp - compare two strings
+ * @str1: the first string
+ * @str2: the second string
+ * @num: the max number of bytes to compare
  *
- * Return: 0 if the first n bytes of s1 and s2 are equal, otherwise non-zero
+ * Return: 0 if the first num bytes of
+ * str1 and str2 are equal, otherwise non-zero
  */
-int _strncmp(const char *s1, const char *s2, size_t n)
+int customStrncmp(const char *str1, const char *str2, size_t num)
 {
-	for (; n && *s1 && *s2; --n, ++s1, ++s2)
+	for (; num && *str1 && *str2; --num, ++str1, ++str2)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
+		if (*str1 != *str2)
+			return (*str1 - *str2);
 	}
-	if (n)
+	if (num)
 	{
-		if (*s1)
+		if (*str1)
 			return (1);
-		if (*s2)
+		if (*str2)
 			return (-1);
 	}
 	return (0);
 }
 
 /**
- * _close - close a file descriptor and print an error message upon failure
- * @fd: the file descriptor to close
+ * customClose - close a file descriptor and
+ * print an error message upon failure
+ * @fileDescriptor: the file descriptor to close
  */
-void _close(int fd)
+void customClose(int fileDescriptor)
 {
-	if (close(fd) != -1)
+	if (close(fileDescriptor) != -1)
 		return;
 	write(STDERR_FILENO, "Error: Can't close fd\n", 22);
 	exit(98);
 }
 
 /**
- * _read - read from a file and print an error message upon failure
- * @fd: the file descriptor to read from
- * @buf: the buffer to write to
+ * customRead - read from a file and print an error message upon failure
+ * @fileDescriptor: the file descriptor to read from
+ * @buffer: the buffer to write to
  * @count: the number of bytes to read
  */
-void _read(int fd, char *buf, size_t count)
+void customRead(int fileDescriptor, char *buffer, size_t count)
 {
-	if (read(fd, buf, count) != -1)
+	if (read(fileDescriptor, buffer, count) != -1)
 		return;
 	write(STDERR_FILENO, "Error: Can't read from file\n", 28);
-	_close(fd);
+	customClose(fileDescriptor);
 	exit(98);
 }
 
 /**
- * elf_magic - print ELF magic
- * @buffer: the ELF header
+ * printElfMagic - print ELF magic
+ * @elfHeader: the ELF header
  */
-void elf_magic(const unsigned char *buffer)
+void printElfMagic(const unsigned char *elfHeader)
 {
 	unsigned int i;
 
-	if (_strncmp((const char *)buffer, ELFMAG, 4))
+	if (customStrncmp((const char *)elfHeader, ELFMAG, 4))
 	{
 		write(STDERR_FILENO, "Error: Not an ELF file\n", 23);
 		exit(98);
@@ -73,187 +75,11 @@ void elf_magic(const unsigned char *buffer)
 	printf("ELF Header:\n  Magic:   ");
 
 	for (i = 0; i < 16; ++i)
-		printf("%02x%c", buffer[i], i < 15 ? ' ' : '\n');
+		printf("%02x%c", elfHeader[i], i < 15 ? ' ' : '\n');
 }
 
 /**
- * elf_class - print ELF class
- * @buffer: the ELF header
- *
- * Return: bit mode (32 or 64)
- */
-size_t elf_class(const unsigned char *buffer)
-{
-	printf("  %-34s ", "Class:");
-
-	if (buffer[EI_CLASS] == ELFCLASS64)
-	{
-		printf("ELF64\n");
-		return (64);
-	}
-	if (buffer[EI_CLASS] == ELFCLASS32)
-	{
-		printf("ELF32\n");
-		return (32);
-	}
-	printf("<unknown: %x>\n", buffer[EI_CLASS]);
-	return (32);
-}
-
-/**
- * elf_data - print ELF data
- * @buffer: the ELF header
- *
- * Return: 1 if big endian, otherwise 0
- */
-int elf_data(const unsigned char *buffer)
-{
-	printf("  %-34s ", "Data:");
-
-	if (buffer[EI_DATA] == ELFDATA2MSB)
-	{
-		printf("2's complement, big endian\n");
-		return (1);
-	}
-	if (buffer[EI_DATA] == ELFDATA2LSB)
-	{
-		printf("2's complement, little endian\n");
-		return (0);
-	}
-	printf("Invalid data encoding\n");
-	return (0);
-}
-
-/**
- * elf_version - print ELF version
- * @buffer: the ELF header
- */
-void elf_version(const unsigned char *buffer)
-{
-	printf("  %-34s %u", "Version:", buffer[EI_VERSION]);
-
-	if (buffer[EI_VERSION] == EV_CURRENT)
-		printf(" (current)\n");
-	else
-		printf("\n");
-}
-
-/**
- * elf_osabi - print ELF OS/ABI
- * @buffer: the ELF header
- */
-void elf_osabi(const unsigned char *buffer)
-{
-	const char *os_table[19] = {
-	    "UNIX - System V",
-	    "UNIX - HP-UX",
-	    "UNIX - NetBSD",
-	    "UNIX - GNU",
-	    "<unknown: 4>",
-	    "<unknown: 5>",
-	    "UNIX - Solaris",
-	    "UNIX - AIX",
-	    "UNIX - IRIX",
-	    "UNIX - FreeBSD",
-	    "UNIX - Tru64",
-	    "Novell - Modesto",
-	    "UNIX - OpenBSD",
-	    "VMS - OpenVMS",
-	    "HP - Non-Stop Kernel",
-	    "AROS",
-	    "FenixOS",
-	    "Nuxi CloudABI",
-	    "Stratus Technologies OpenVOS"};
-
-	printf("  %-34s ", "OS/ABI:");
-
-	if (buffer[EI_OSABI] < 19)
-		printf("%s\n", os_table[(unsigned int)buffer[EI_OSABI]]);
-	else
-		printf("<unknown: %x>\n", buffer[EI_OSABI]);
-}
-
-/**
- * elf_abivers - print ELF ABI version
- * @buffer: the ELF header
- */
-void elf_abivers(const unsigned char *buffer)
-{
-	printf("  %-34s %u\n", "ABI Version:", buffer[EI_ABIVERSION]);
-}
-
-/**
- * elf_type - print ELF type
- * @buffer: the ELF header
- * @big_endian: endianness (big endian if non-zero)
- */
-void elf_type(const unsigned char *buffer, int big_endian)
-{
-	char *type_table[5] = {
-	    "NONE (No file type)",
-	    "REL (Relocatable file)",
-	    "EXEC (Executable file)",
-	    "DYN (Shared object file)",
-	    "CORE (Core file)"};
-	unsigned int type;
-
-	printf("  %-34s ", "Type:");
-
-	if (big_endian)
-		type = 0x100 * buffer[16] + buffer[17];
-	else
-		type = 0x100 * buffer[17] + buffer[16];
-
-	if (type < 5)
-		printf("%s\n", type_table[type]);
-	else if (type >= ET_LOOS && type <= ET_HIOS)
-		printf("OS Specific: (%4x)\n", type);
-	else if (type >= ET_LOPROC && type <= ET_HIPROC)
-		printf("Processor Specific: (%4x)\n", type);
-	else
-		printf("<unknown: %x>\n", type);
-}
-
-/**
- * elf_entry - print entry point address
- * @buffer: string containing the entry point address
- * @bit_mode: bit mode (32 or 64)
- * @big_endian: endianness (big endian if non-zero)
- */
-void elf_entry(const unsigned char *buffer, size_t bit_mode, int big_endian)
-{
-	int address_size = bit_mode / 8;
-
-	printf("  %-34s 0x", "Entry point address:");
-
-	if (big_endian)
-	{
-		while (address_size && !*(buffer))
-			--address_size, ++buffer;
-
-		printf("%x", *buffer & 0xff);
-
-		while (--address_size > 0)
-			printf("%02x", *(++buffer) & 0xff);
-	}
-	else
-	{
-		buffer += address_size;
-
-		while (address_size && !*(--buffer))
-			--address_size;
-
-		printf("%x", *buffer & 0xff);
-
-		while (--address_size > 0)
-			printf("%02x", *(--buffer) & 0xff);
-	}
-
-	printf("\n");
-}
-
-/**
- * main - copy a file's contents to another file
+ * main - analyze an ELF file header
  * @argc: the argument count
  * @argv: the argument values
  *
@@ -261,10 +87,13 @@ void elf_entry(const unsigned char *buffer, size_t bit_mode, int big_endian)
  */
 int main(int argc, const char *argv[])
 {
-	unsigned char buffer[18];
-	unsigned int bit_mode;
-	int big_endian;
-	int fd;
+	unsigned char headerBuffer[18];
+
+	unsigned int bitMode;
+
+	int isBigEndian;
+
+	int fileDescriptor;
 
 	if (argc != 2)
 	{
@@ -272,29 +101,29 @@ int main(int argc, const char *argv[])
 		exit(98);
 	}
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	fileDescriptor = open(argv[1], O_RDONLY);
+	if (fileDescriptor == -1)
 	{
 		write(STDERR_FILENO, "Error: Can't read from file\n", 28);
 		exit(98);
 	}
 
-	_read(fd, (char *)buffer, 18);
+	customRead(fileDescriptor, (char *)headerBuffer, 18);
 
-	elf_magic(buffer);
-	bit_mode = elf_class(buffer);
-	big_endian = elf_data(buffer);
-	elf_version(buffer);
-	elf_osabi(buffer);
-	elf_abivers(buffer);
-	elf_type(buffer, big_endian);
+	printElfMagic(headerBuffer);
+	bitMode = elfClass(headerBuffer);
+	isBigEndian = elfData(headerBuffer);
+	elfVersion(headerBuffer);
+	elfOsabi(headerBuffer);
+	elfAbiVersion(headerBuffer);
+	elfType(headerBuffer, isBigEndian);
 
-	lseek(fd, 24, SEEK_SET);
-	_read(fd, (char *)buffer, bit_mode / 8);
+	lseek(fileDescriptor, 24, SEEK_SET);
+	customRead(fileDescriptor, (char *)headerBuffer, bitMode / 8);
 
-	elf_entry(buffer, bit_mode, big_endian);
+	elfEntry(headerBuffer, bitMode, isBigEndian);
 
-	_close(fd);
+	customClose(fileDescriptor);
 
 	return (0);
 }
