385aa6333300e4d21b3bf1b3349267fe99db5f24
Youssef-Hassane <Youssef_Hassane@icloud.com>
2024-03-20 12:32:05 +0000

    Task #5 - DONE

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A function def island_perimeter(grid): that
+returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(island_grid):
+    """
+    Calculates the perimeter of an island
+    """
+
+    # Initialize variables to track island borders and island_size
+    island_border_count = 0
+    island_size = 0
+
+    # Iterate over each cell in the island island_grid
+    for row in range(len(island_grid)):
+        for col in range(len(island_grid[0])):
+            # Check if the current cell is part of an island
+            if island_grid[row][col] == 1:
+                island_size += 1
+                if (col > 0 and island_grid[row][col - 1] == 1):
+                    island_border_count += 1
+                if (row > 0 and island_grid[row - 1][col] == 1):
+                    island_border_count += 1
+
+    # Calculate the perimeter of the island
+    result = island_size * 4 - island_border_count * 2
+    return result
