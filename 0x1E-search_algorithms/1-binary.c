1a4aa949cc4251eb813956a2d0013acd466671fd
Youssef-Hassane <youssef_hassane@icloud.com>
2024-05-09 02:24:16 +0300

    Task #1 - DONE

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,7 +7,7 @@
  * --------------------------
  * Prototype: int binary_search(int *array, size_t size, int value);
  * --------------------------
- * @arr: array of integers
+ * @array: array of integers
  * @size: size of the array
  * @target: value to search for
  * --------------------------
@@ -17,46 +17,35 @@
  * By Youssef Hassane
  */
 
-int binary_search(int *arr, size_t size, int target)
+int binary_search(int *array, size_t size, int target)
 {
-	size_t start = 0;
-	size_t end = size - 1;
-	size_t i;
-	/* Print the array */
-	printf("Searching in array: ");
-	for (i = 0; i < size; i++)
-	{
-		printf("%d", arr[i]);
-		if (i != size - 1)
-			printf(", ");
-	}
-	printf("\n");
+	size_t left = 0, right = size - 1, mid;
 
-	while (start <= end)
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
 	{
-		/* Calculate the index to search in */
-		size_t index = (start + end) / 2;
 		size_t i;
-		/* Print the subarray being searched */
+
 		printf("Searching in array: ");
-		for (i = start; i <= end; i++)
+		for (i = left; i <= right; i++)
 		{
-			printf("%d", arr[i]);
-			if (i != end)
+			if (i != left)
 				printf(", ");
+			printf("%d", array[i]);
 		}
 		printf("\n");
 
-		/* Check if element matches target */
-		if (arr[index] == target)
-			return (index);
+		mid = (left + right) / 2;
 
-		/* Search in the appropriate half */
-		if (arr[index] < target)
-			start = index + 1;
+		if (array[mid] == target)
+			return (mid);
+		else if (array[mid] < target)
+			left = mid + 1;
 		else
-			end = index - 1;
+			right = mid - 1;
 	}
-	/* if not found, return -1 */
+
 	return (-1);
 }
