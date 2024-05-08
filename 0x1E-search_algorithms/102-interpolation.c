92789f78edc91e2a6224d4fb75befd180fa615f9
Youssef-Hassane <youssef_hassane@icloud.com>
2024-05-09 02:42:07 +0300

    Task #9 - DONE

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -8,38 +8,57 @@
  * Prototype: int interpolation_search(int *array, size_t size, int target);
  * --------------------------
  * @array: array of integers
- * @size: size of the array
- * @value: value to search for
+ * @count: number of elements in array
+ * @value_to_search: value to search for
  * --------------------------
  * Description:
- * Finds a target in a sorted array of integers using
+ * Finds a value in a sorted array of integers using
  * Interpolation search algorithm
  * --------------------------
  * By Youssef Hassane
  */
 
-int interpolation_search(int *array, size_t size, int value)
+int interpolation_search(int *arr, size_t count, int value_to_search)
 {
-	size_t low = 0, high = size - 1;
+	unsigned int left_index = 0;
+	unsigned int right_index = count - 1;
+	unsigned int index_to_check;
 
-	if (array == NULL)
+	if (arr == NULL || count == 0)
 		return (-1);
 
-	while (low <= high && value >= array[low] && value <= array[high])
+	while (arr[left_index] != arr[right_index])
 	{
-		size_t pos = low + (((double)(high - low) / (array[high] - array[low]))
-		* (value - array[low]));
+		/* calculate the position to check based on interpolation formula */
+		index_to_check = left_index +
+		(unsigned int)((double)(right_index - left_index) /
+		(arr[right_index] - arr[left_index]) *
+		(value_to_search - arr[left_index]));
 
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		/* check if the index is out of range */
+		if (index_to_check >= count)
+		{
+			printf("Value checked array[%u] is out of range\n",
+				index_to_check);
+			return (-1);
+		}
 
-		if (array[pos] == value)
-			return (pos);
-		else if (array[pos] < value)
-			low = pos + 1;
+		/* print the value checked at the index */
+		printf("Value checked array[%u] = [%d]\n", index_to_check,
+			arr[index_to_check]);
+
+		/* update the range based on the value checked */
+		if (value_to_search < arr[index_to_check])
+			right_index = index_to_check - 1;
+		else if (value_to_search > arr[index_to_check])
+			left_index = index_to_check + 1;
 		else
-			high = pos - 1;
+			return (index_to_check);
 	}
 
-	printf("Value checked array[%lu] is out of range\n", high);
-	return (-1);
+	if (value_to_search == arr[left_index])
+		return (left_index);
+	else
+		return (-1);
 }
+
