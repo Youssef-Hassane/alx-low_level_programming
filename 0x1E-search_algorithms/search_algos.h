f1888545b5289c57e328736493987839198b8f4f
Youssef-Hassane <youssef_hassane@icloud.com>
2024-05-09 04:08:19 +0300

    Task #13 - DONE

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -30,4 +30,24 @@ typedef struct listint_s
 
 listint_t *jump_list(listint_t *head, size_t size, int target);
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *head, int target);
+
 #endif
