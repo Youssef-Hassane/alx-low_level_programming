aaa4c080ab7ee59ed746d4bc655f22f16d74f9cc
Youssef-Hassane <youssef_hassane@icloud.com>
2024-05-09 04:03:43 +0300

    Task #12 - DONE

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,4 +12,22 @@ int interpolation_search(int *array, size_t size, int target);
 int exponential_search(int *arr, size_t arrSize, int target);
 int advanced_binary(int *arr, size_t size, int target);
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *jump_list(listint_t *head, size_t size, int target);
+
 #endif
