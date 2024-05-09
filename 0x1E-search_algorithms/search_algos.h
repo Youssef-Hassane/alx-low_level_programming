bc2f6b8decb53e964a3ae94c48ffb4b850521c2f
Youssef-Hassane <youssef_hassane@icloud.com>
2024-05-09 03:57:10 +0300

    Task #11 - DONE

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int target);
 
 int exponential_search(int *arr, size_t arrSize, int target);
-
+int advanced_binary(int *arr, size_t size, int target);
 
 #endif
